fc09e95be809d4ca6af9c10cfcd0e6b4e7149257
Kostas Makedos <kostas.makedos@gmail.com>
2025-06-26 01:51:29 +0300

    Change port

--- app/src/start.sh
+++ app/src/start.sh
@@ -3,7 +3,7 @@ echo "Starting application"
 # BACKEND_HOSTNAME is needed so Flask can listen to any ip
 # We pass this from the Makefile as env var
 BACKEND_HOSTNAME=${BACKEND_HOSTNAME:-127.0.0.1}
-BACKEND_PORT=${BACKEND_PORT:-5000}
+BACKEND_PORT=${BACKEND_PORT:-8000}
 # Debug level for Flask
 BACKEND_DEBUG=${BACKEND_DEBUG:-""}
 flask run -h $BACKEND_HOSTNAME -p $BACKEND_PORT $BACKEND_DEBUG
