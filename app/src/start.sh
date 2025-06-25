fd7fb487bb99aca836059242732db7dec37efb84
Kostas Makedos <kostas.makedos@gmail.com>
2025-06-26 02:32:40 +0300

    Correct db

--- app/src/start.sh
+++ app/src/start.sh
@@ -2,7 +2,7 @@
 echo "Starting application"
 # BACKEND_HOSTNAME is needed so Flask can listen to any ip
 # We pass this from the Makefile as env var
-BACKEND_HOSTNAME=${BACKEND_HOSTNAME:-127.0.0.1}
+BACKEND_HOSTNAME=${BACKEND_HOSTNAME:-0.0.0.0}
 BACKEND_PORT=${BACKEND_PORT:-8000}
 # Debug level for Flask
 BACKEND_DEBUG=${BACKEND_DEBUG:-""}
